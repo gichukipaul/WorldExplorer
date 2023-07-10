2a404177bd03b5a36448e7527fb265882877f1b6
Paul Gichuki <gichukipaul043@outlook.com>
2023-07-10 15:15:53 +0300

    Added some Unit tests

---
+++ CountryExplorerTests/ViewControllerViewModelTests.swift
@@ -0,0 +1,20 @@
+//
+//  ViewControllerViewModelTests.swift
+//  CountryExplorerTests
+//
+//  Created by user on 10/07/2023.
+//
+
+import Foundation
+import XCTest
+@testable import CountryExplorer
+
+class ViewControllerViewModelTests: XCTestCase {
+    
+    let vm = ViewControllerViewModel()
+    
+    func testInitialData() throws {
+        XCTAssertEqual(vm.numberOfContinents(), 0)
+    }
+    
+}
